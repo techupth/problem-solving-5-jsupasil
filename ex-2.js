d0d71973c2afa0679fcbf25da25c307bc0f9c5d8
CharlieHii <charlie_chaper@hotmail.com>
2024-02-29 23:42:02 +0700

    feat: answer

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,16 @@
 function moveZeroes(nums) {
-    
-};
+  let newNums = nums.filter((num) => {
+    return num;
+  });
+
+  let zeroCounts = nums.length - newNums.length;
+  let i = 0;
+  while (i < zeroCounts) {
+    newNums.push(0);
+    i++;
+  }
+  return newNums;
+}
+
+let nums = [0, 1, 0, 3, 12];
+console.log(moveZeroes(nums));
